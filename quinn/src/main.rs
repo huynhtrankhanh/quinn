f97cc6fe2c45211359007248dcb70cc0997e8854
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2020-09-27 08:14:33 +0000

    QUIC interpreter is done

--- quinn/src/main.rs
+++ quinn/src/main.rs
@@ -161,92 +161,422 @@ use std::{error::Error, net::SocketAddr, sync::Arc};
 
 use crate::{ClientConfig, ClientConfigBuilder, Endpoint};
 use async_std::prelude::*;
+use async_std::sync::channel;
 
-use std::collections::HashMap;
+use sodiumoxide::crypto::{box_, hash};
 
-use sodiumoxide::crypto::{box_, sign};
+use clap::Clap;
 
-fn main() {
-    task::block_on(async move {
-        use async_std::io::{self, BufReader};
-        let mut incoming_data = BufReader::new(io::stdin());
+use async_std::os::unix::net::UnixStream;
 
-        let mut connections = HashMap::<SocketAddr, crate::Connection>::new();
+use futures::AsyncReadExt;
 
-        loop {
-            let mut line = String::new();
+use dashmap::DashMap;
 
-            incoming_data.read_line(&mut line).await.unwrap();
+fn synthesize_socket_address(public_encryption_key: &box_::PublicKey) -> SocketAddr {
+    let mut hash_state = hash::State::new();
+    hash_state.update(public_encryption_key.as_ref());
+    let digest = hash_state.finalize();
+    let digest = digest.as_ref();
 
-            let blob = base64::decode(line).unwrap();
+    (
+        [
+            digest[0], digest[1], digest[2], digest[3], digest[4], digest[5], digest[6], digest[7],
+            digest[8], digest[9], digest[10], digest[11], digest[12], digest[13], digest[14],
+            digest[15],
+        ],
+        0,
+    )
+        .into()
+}
 
-            let mut counter: usize = 0;
+#[tokio::main]
+async fn main() {
+    #[derive(Clap)]
+    #[clap(
+        version = "1.0",
+        author = "Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>"
+    )]
+    struct Options {
+        #[clap(long)]
+        server_socket: String,
+        #[clap(long)]
+        client_socket: String,
+    }
 
-            let mut advance = |count: usize| {
-                let left = counter;
-                counter += count;
-                if counter > blob.len() {
-                    return None;
+    let options = Arc::new(Options::parse());
+    use async_std::io::{self, BufReader};
+    let mut incoming_data = BufReader::new(io::stdin());
+
+    let connections = Arc::new(DashMap::<
+        SocketAddr,
+        Sender<(Vec<u8>, SocketAddr, Option<EcnCodepoint>)>,
+    >::new());
+
+    let map_synthetic_address_to_public_key =
+        Arc::new(DashMap::<SocketAddr, box_::PublicKey>::new());
+
+    let (my_public_encryption_key, my_private_encryption_key) = box_::gen_keypair();
+
+    let my_private_encryption_key = Arc::new(my_private_encryption_key);
+
+    let (my_server_public_encryption_key, my_server_private_encryption_key) = box_::gen_keypair();
+
+    let my_server_private_encryption_key = Arc::new(my_server_private_encryption_key);
+    let my_server_public_encryption_key = Arc::new(my_server_public_encryption_key);
+
+    let (transmit_incoming_server, receive_incoming_server) = channel(1);
+    let (transmit_outgoing_server, receive_outgoing_server) = channel::<Transmit>(1);
+
+    {
+        let my_server_public_encryption_key = my_server_public_encryption_key.clone();
+
+        tokio::spawn(async move {
+            loop {
+                use std::time::Duration;
+
+                {
+                    let mut message = Vec::new();
+
+                    // Version byte set to 0
+                    message.extend_from_slice(&[0u8]);
+
+                    // Message type set to Advertisement
+                    message.extend_from_slice(&[0u8]);
+
+                    message.extend_from_slice(my_server_public_encryption_key.as_ref().as_ref());
+
+                    println!("{}", base64::encode(message));
                 }
-                Some(&blob[left..counter])
-            };
 
-            let version = match advance(1) {
-                Some(it) => it[0],
-                None => continue,
-            };
-
-            if version != 0 {
-                // First byte indicates version. This is the first revision
-                // of the tunneling protocol, so the version byte is expected
-                // to be zero. The incoming packet is discarded.
-                continue;
+                task::sleep(Duration::from_secs(1)).await;
             }
+        });
+    }
 
-            let message_type = match advance(1) {
-                Some(it) => it[0],
-                None => continue,
-            };
+    {
+        let my_server_private_encryption_key = my_server_private_encryption_key.clone();
+        let map_synthetic_address_to_public_key = map_synthetic_address_to_public_key.clone();
+        tokio::spawn(async move {
+            while let Ok(outgoing) = receive_outgoing_server.recv().await {
+                let blob = outgoing.contents;
 
-            enum MessageType {
-                Advertisement,
-                Packet,
-            };
+                let mut message = Vec::new();
 
-            use MessageType::*;
+                // Version byte set to 0
+                message.extend_from_slice(&[0u8]);
 
-            let message_type = match message_type {
-                0 => Advertisement,
-                1 => Packet,
-                _ => continue,
-            };
+                // Message type set to Packet
+                message.extend_from_slice(&[1u8]);
 
-            match message_type {
-                Advertisement => {
-                    let public_encryption_key = advance(box_::PUBLICKEYBYTES);
-                    let public_signing_key = advance(sign::PUBLICKEYBYTES);
+                message.extend_from_slice(my_server_public_encryption_key.as_ref().as_ref());
 
-                    if let Some(_) = advance(1) {
-                        // Discard the packet as it contains additional garbage.
-                        continue;
-                    }
+                let encrypted_section = {
+                    let mut encrypted_section = Vec::new();
 
-                    let public_encryption_key = match public_encryption_key {
-                        Some(it) => box_::PublicKey::from_slice(it),
-                        None => continue,
+                    let nonce = box_::gen_nonce();
+                    encrypted_section.extend_from_slice(nonce.as_ref());
+
+                    let ciphertext = box_::seal(
+                        &blob,
+                        &nonce,
+                        &match map_synthetic_address_to_public_key.get(&outgoing.destination) {
+                            Some(it) => *it,
+                            None => continue,
+                        },
+                        &my_server_private_encryption_key,
+                    );
+
+                    encrypted_section.extend_from_slice(ciphertext.as_ref());
+
+                    encrypted_section
+                };
+
+                message.extend_from_slice(encrypted_section.as_ref());
+
+                println!("{}", base64::encode(message));
+            }
+        });
+    }
+
+    {
+        let options = options.clone();
+        let map_synthetic_address_to_public_key = map_synthetic_address_to_public_key.clone();
+        tokio::spawn(async move {
+            let (mut incoming, _server_cert) =
+                make_server_endpoint((transmit_outgoing_server, receive_incoming_server)).unwrap();
+
+            while let Some(connection) = incoming.next().await {
+                let mut connection = match connection.await {
+                    Ok(it) => it,
+                    Err(_) => continue,
+                };
+
+                let synthetic_address = connection.connection.remote_address();
+
+                while let Some(stream) = connection.bi_streams.next().await {
+                    let (send, receive) = match stream {
+                        Ok(it) => it,
+                        Err(_) => break,
                     };
 
-                    let public_signing_key = match public_signing_key {
-                        Some(it) => sign::PublicKey::from_slice(it),
-                        None => continue,
-                    };
+                    let stream = UnixStream::connect(&options.server_socket).await.unwrap();
 
-                    // Check hash table: if connection doesn't exist, create one                    
+                    let (reader, writer) = stream.split();
+
+                    tokio::spawn(async move {
+                        io::copy(reader, send).await.ok();
+                    });
+
+                    tokio::spawn(async move {
+                        io::copy(receive, writer).await.ok();
+                    });
                 }
-                Packet => {}
-            };
+
+                map_synthetic_address_to_public_key.remove(&synthetic_address);
+            }
+        });
+    }
+
+    loop {
+        let mut line = String::new();
+
+        incoming_data.read_line(&mut line).await.unwrap();
+
+        let blob = base64::decode(line).unwrap();
+
+        let mut counter: usize = 0;
+
+        let mut advance = |count: usize| {
+            let left = counter;
+            counter += count;
+            if counter > blob.len() {
+                return None;
+            }
+            Some(&blob[left..counter])
+        };
+
+        let version = match advance(1) {
+            Some(it) => it[0],
+            None => continue,
+        };
+
+        if version != 0 {
+            // First byte indicates version. This is the first revision
+            // of the tunneling protocol, so the version byte is expected
+            // to be zero. The incoming packet is discarded.
+            continue;
         }
-    });
+
+        let message_type = match advance(1) {
+            Some(it) => it[0],
+            None => continue,
+        };
+
+        enum MessageType {
+            Advertisement,
+            Packet,
+        };
+
+        use MessageType::*;
+
+        let message_type = match message_type {
+            0 => Advertisement,
+            1 => Packet,
+            _ => continue,
+        };
+
+        match message_type {
+            Advertisement => {
+                let public_encryption_key = advance(box_::PUBLICKEYBYTES);
+
+                if let Some(_) = advance(1) {
+                    // Discard the packet as it contains additional garbage.
+                    continue;
+                }
+
+                let public_encryption_key = match public_encryption_key {
+                    Some(it) => box_::PublicKey::from_slice(it).unwrap(),
+                    None => continue,
+                };
+
+                let synthetic_address = synthesize_socket_address(&public_encryption_key);
+
+                if connections.contains_key(&synthetic_address) {
+                    continue;
+                }
+
+                let (transmit_incoming, receive_incoming) = channel(1);
+                connections.insert(synthetic_address.clone(), transmit_incoming);
+
+                let (transmit_outgoing, receive_outgoing) = channel(1);
+
+                let connections = connections.clone();
+                let options = options.clone();
+                tokio::spawn((|| async move {
+                    let client_cfg = configure_client();
+                    let mut endpoint_builder = Endpoint::builder();
+                    endpoint_builder.default_client_config(client_cfg);
+
+                    let (endpoint, _) =
+                        match endpoint_builder.with_socket((transmit_outgoing, receive_incoming)) {
+                            Ok(it) => it,
+                            Err(_) => {
+                                connections.remove(&synthetic_address);
+                                return;
+                            }
+                        };
+                    let connection = match endpoint.connect(&synthetic_address, "localhost") {
+                        Ok(it) => it,
+                        Err(_) => {
+                            connections.remove(&synthetic_address);
+                            return;
+                        }
+                    }
+                    .await;
+
+                    let crate::NewConnection { connection, .. } = match connection {
+                        Ok(it) => it,
+                        Err(_) => {
+                            connections.remove(&synthetic_address);
+                            return;
+                        }
+                    };
+
+                    let (send, receive) = match connection.open_bi().await {
+                        Ok(it) => it,
+                        Err(_) => {
+                            connections.remove(&synthetic_address);
+                            return;
+                        }
+                    };
+
+                    let stream = UnixStream::connect(&options.client_socket).await.unwrap();
+
+                    let (reader, writer) = stream.split();
+
+                    tokio::spawn(async move {
+                        io::copy(reader, send).await.ok();
+                    });
+
+                    tokio::spawn(async move {
+                        io::copy(receive, writer).await.ok();
+                    });
+
+                    endpoint.wait_idle().await;
+
+                    connections.remove(&synthetic_address);
+                })());
+
+                let my_private_encryption_key = my_private_encryption_key.clone();
+                tokio::spawn(async move {
+                    while let Ok(outgoing) = receive_outgoing.recv().await {
+                        let blob = outgoing.contents;
+
+                        let mut message = Vec::new();
+
+                        // Version byte set to 0
+                        message.extend_from_slice(&[0u8]);
+
+                        // Message type set to Packet
+                        message.extend_from_slice(&[1u8]);
+
+                        message.extend_from_slice(my_public_encryption_key.as_ref());
+
+                        let encrypted_section = {
+                            let mut encrypted_section = Vec::new();
+
+                            let nonce = box_::gen_nonce();
+                            encrypted_section.extend_from_slice(nonce.as_ref());
+
+                            let ciphertext = box_::seal(
+                                &blob,
+                                &nonce,
+                                &public_encryption_key,
+                                &my_private_encryption_key,
+                            );
+
+                            encrypted_section.extend_from_slice(ciphertext.as_ref());
+
+                            encrypted_section
+                        };
+
+                        message.extend_from_slice(encrypted_section.as_ref());
+
+                        println!("{}", base64::encode(message));
+                    }
+                });
+            }
+            Packet => {
+                let public_encryption_key = advance(box_::PUBLICKEYBYTES);
+
+                let public_encryption_key = match public_encryption_key {
+                    Some(it) => box_::PublicKey::from_slice(&it).unwrap(),
+                    None => continue,
+                };
+
+                let nonce = advance(box_::NONCEBYTES);
+
+                let nonce = match nonce {
+                    Some(it) => box_::Nonce::from_slice(&it).unwrap(),
+                    None => continue,
+                };
+
+                let encrypted_section = &blob[counter..];
+
+                let packet_is_sent_to_server = (|| async {
+                    let plaintext = box_::open(
+                        &encrypted_section,
+                        &nonce,
+                        &public_encryption_key,
+                        &my_server_private_encryption_key,
+                    );
+
+                    let plaintext = match plaintext {
+                        Ok(it) => it,
+                        Err(_) => return false,
+                    };
+
+                    let synthetic_address = synthesize_socket_address(&public_encryption_key);
+
+                    map_synthetic_address_to_public_key
+                        .insert(synthetic_address.clone(), public_encryption_key);
+
+                    transmit_incoming_server
+                        .send((plaintext, synthetic_address, None))
+                        .await;
+
+                    true
+                })()
+                .await;
+
+                if packet_is_sent_to_server {
+                    continue;
+                }
+
+                let plaintext = box_::open(
+                    &encrypted_section,
+                    &nonce,
+                    &public_encryption_key,
+                    &my_private_encryption_key,
+                );
+
+                let plaintext = match plaintext {
+                    Ok(it) => it,
+                    Err(_) => continue,
+                };
+
+                let synthetic_address = synthesize_socket_address(&public_encryption_key);
+
+                match connections.get(&synthetic_address) {
+                    Some(it) => {
+                        it.send((plaintext, synthetic_address, None)).await;
+                    }
+                    None => continue,
+                };
+            }
+        };
+    }
 }
 
 fn configure_server() -> Result<(ServerConfig, Vec<u8>), Box<dyn Error>> {
@@ -277,47 +607,6 @@ fn make_server_endpoint(
     endpoint_builder.listen(server_config);
     let (_endpoint, incoming) = endpoint_builder.with_socket(socket)?;
     Ok((incoming, server_cert))
-}
-
-/// Runs a QUIC server bound to given address.
-async fn run_server(
-    socket: (
-        Sender<Transmit>,
-        Receiver<(Vec<u8>, SocketAddr, Option<EcnCodepoint>)>,
-    ),
-) {
-    let (mut incoming, _server_cert) = make_server_endpoint(socket).unwrap();
-    // accept a single connection
-    let incoming_conn = incoming.next().await.unwrap();
-    let new_conn = incoming_conn.await.unwrap();
-    println!(
-        "[server] connection accepted: addr={}",
-        new_conn.connection.remote_address()
-    );
-}
-
-async fn run_client(
-    server_addr: SocketAddr,
-    socket: (
-        Sender<Transmit>,
-        Receiver<(Vec<u8>, SocketAddr, Option<EcnCodepoint>)>,
-    ),
-) -> crate::Connection {
-    let client_cfg = configure_client();
-    let mut endpoint_builder = Endpoint::builder();
-    endpoint_builder.default_client_config(client_cfg);
-
-    let (endpoint, _) = endpoint_builder.with_socket(socket).unwrap();
-
-    // connect to server
-    let crate::NewConnection { connection, .. } = endpoint
-        .connect(&server_addr, "localhost")
-        .unwrap()
-        .await
-        .unwrap();
-    println!("[client] connected: addr={}", connection.remote_address());
-    
-    connection
 }
 
 /// Dummy certificate verifier that treats any certificate as valid.
